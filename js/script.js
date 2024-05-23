ebecfcf52b883b6bb4bf617b8a095885f2b44bde
YvanBlanchette <yvanblanchette@gmail.com>
2024-05-23 14:01:58 -0400

    Responsive website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-//HEADER CHANGE ON SCROLL FUNCTIONALITY
+// !---> HEADER CHANGE ON SCROLL FUNCTIONALITY <---! //
 const header = document.querySelector('.header');
+const logoContainer = document.querySelector('.header__logo-container');
+const navMenu = document.querySelector('.header__nav');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 400) {
@@ -10,7 +12,34 @@ window.addEventListener('scroll', () => {
 });
 
 
-// INFO COUNTERS FUNCTIONALITY
+
+// !---> SECTION SCROLL FUNCTIONALITY <---! //
+const sections = document.querySelectorAll('section');
+
+const options = {
+  root: null, // relative to the viewport
+  rootMargin: '0px',
+  threshold: 0.3 // 30% of the section is visible
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+
+
+
+// !---> INFO COUNTERS FUNCTIONALITY <---! //
 const counters = document.querySelectorAll('.js-counter');
 const countersContainer = document.querySelector('.js-counters-container');
 
@@ -62,7 +91,7 @@ window.addEventListener('scroll', () => {
 
 
 
-// STARBASE CARDS FUNCTIONALITY
+// !---> STARBASE CARDS FUNCTIONALITY <---! //
 const starbaseCards = document.querySelectorAll('.starbase__card');
 
 starbaseCards.forEach((card) => {
@@ -82,3 +111,99 @@ starbaseCards.forEach((card) => {
     }
   });
 });
+
+
+
+// !---> MOBILE NAV FUNCTIONALITY <---! //
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileNav = document.getElementById('nav-menu');
+const navLink = document.querySelectorAll('.nav__link');
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileNav.classList.toggle('show');
+})
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.remove('show');
+  })
+})
+
+
+// !---> FORM SUBMISSION & VALIDATION <---! //
+const form = document.getElementById('form');
+const first_name = document.getElementById('first_name');
+const last_name = document.getElementById('last_name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const validForm = formValidation()
+
+  if (validForm) {
+    window.location.href = "message-confirmation.html";
+  }
+}
+
+const formValidation = () => {
+  let noError = true;
+
+  const first_nameVal = first_name.value.trim();
+  const last_nameVal = last_name.value.trim();
+  const emailVal = email.value.trim();
+  const messageVal = message.value.trim();
+
+  if (first_nameVal === '') {
+    setError(first_name, 'SVP inscrire votre prénom...');
+    noError = false;
+  } else {
+    setSuccess(first_name);
+  }
+
+  if (last_nameVal === '') {
+    setError(last_name, 'SVP inscrire votre nom de famille...');
+    noError = false;
+  } else {
+    setSuccess(last_name);
+  }
+
+  if (emailVal === '') {
+    setError(email, 'SVP inscrire votre email...');
+    noError = false;
+  } else if (!isValidEmail(emailVal)) {
+    setError(email, 'Courriel invalide...');
+    noError = false;
+  } else {
+    setSuccess(email);
+  }
+
+  if (messageVal === '') {
+    setError(message, 'SVP inscrire votre message...');
+    noError = false;
+  } else {
+    setSuccess(message);
+  }
+  return noError;
+}
+
+const setError = (input, message) => {
+  const inputControl = input.parentElement;
+  const errorDisplay = inputControl.querySelector('.errorMessage');
+  errorDisplay.innerText = message;
+  inputControl.classList.remove('success');
+  inputControl.classList.add('error');
+}
+
+const setSuccess = (input) => {
+  const inputControl = input.parentElement;
+  const errorDisplay = inputControl.querySelector('.errorMessage');
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+}
